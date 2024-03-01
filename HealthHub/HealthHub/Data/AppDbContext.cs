ba58a5dda8f44253d0fbc4302565403b709f844c
Nikolai Kovalenko <nikolay.kov.al.006@gmail.com>
2024-03-01 15:59:00 +0200

    first commit

---
+++ HealthHub/HealthHub/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using HealthHub.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using static System.Collections.Specialized.BitVector32;
+
+namespace HealthHub.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<AppUser> AppUsers { get; set; }
+
+        //public DbSet<Section> Sections { get; set; }
+
+    }
+}
---
+++ HealthHub/HealthHub/Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HealthHub.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string Surname { get; set; }
+        public string Sex { get; set; }
+        public DateTime Birthday { get; set; }
+        public string? PathToPhoto { get; set; }
+        public bool IsActive { get; set; } // Активный или заблокированый
+        public DateTime? DeleteTime { get; set; }
+        public DateTime RegistrationTime { get; set; }
+    }
+}
